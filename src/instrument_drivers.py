9964af2ce257d3765a383afe16961124b74fd53f
Ven_903 <113065405+Ven903-max@users.noreply.github.com>
2026-06-23 09:00:39 -0400

    Update instrument_drivers.py

--- src/instrument_drivers.py
+++ src/instrument_drivers.py
@@ -1,1 +1,16 @@
 # Instrument driver abstraction layer
+import pyvisa
+
+
+class SCPIInstrument:
+    def __init__(self, resource_manager, resource_string):
+        self.instrument = resource_manager.open_resource(resource_string)
+
+    def write(self, command):
+        self.instrument.write(command)
+
+    def query(self, command):
+        return self.instrument.query(command)
+
+    def close(self):
+        self.instrument.close()
